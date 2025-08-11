e76a59903c9825b1e7feac2e6bdf01cc5564ddd1
Alexandr Korneev <git.user.sanya.korneev.20@bk.ru>
2025-08-11 21:31:40 +0300

    улучшили логику

--- src/main/java/com/stepup/proj/MainApplicantion.java
+++ src/main/java/com/stepup/proj/MainApplicantion.java
@@ -3,8 +3,8 @@ package com.stepup.proj;
 public class MainApplicantion {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("11111");
-        System.out.println(22222);
+        System.out.println("100011");
+        System.out.println(10001);
         System.out.println(2);
         System.out.println(3);
     }
